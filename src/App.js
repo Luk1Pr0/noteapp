62abce306d8084c52d244c11d52f5d6bea7c61fb
Luk1Pr0 <lpytel16@gmail.com>
2021-02-20 14:19:49 +0100

    Added form styling and added state to capture the form inputs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './style/App.css';
 
 // Components
 import Form from './components/Form';
+import NoteList from './components/NoteList';
 
 const App = () => {
 	return (
 		<div className="App">
 			<Form />
+			<NoteList />
 		</div>
 	);
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+	const [input, setInput] = useState({
+		title: '',
+		note: '',
+	});
+
+	const onChange = (e) => {
+		setInput({
+			...input,
+			[e.target.name]: e.target.value,
+		});
+	}
+
 	return (
 		<div className='form-container'>
 			<form>
 				<div className="input-container">
 					<label>Title</label>
-					<input type="text" name='title' />
+					<input type="text" name='title' onChange={onChange} value={input.title} />
 				</div>
 				<div className="input-container">
 					<label>Note</label>
-					<textarea type="text" name='note' />
+					<textarea type="text" name='note' onChange={onChange} value={input.note} />
 				</div>
 				<div className="form__btn__container">
 					<button>Add Note</button>
