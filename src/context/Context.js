f2f6dbb9f155dcae4ac592947659addfd7a297fd
Luk1Pr0 <lpytel16@gmail.com>
2021-03-07 17:12:12 +0100

    Still working on the context api

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,1 +1,11 @@
 import React, { createContext } from 'react';
+
+export const noteContext = createContext();
+
+export const Context = (props) => {
+	return (
+		<Context.Provider value={ }>
+			{props.children}
+		</Context.Provider>
+	);
+}
